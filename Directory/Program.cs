ca9e7a4f8a449eec7edfe0805d1f93f6ed4bb3d6
Nilso Junior <96146165+Nilso97@users.noreply.github.com>
2023-04-27 10:50:23 -0300

    Estudando a classe Directory e DirectoryRead

---
+++ Directory/Program.cs
@@ -0,0 +1,110 @@
+﻿using System.IO;
+
+namespace Curso
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateDirectory();
+
+            CreateFile();
+
+            var origin = Path.Combine(Environment.CurrentDirectory, "brasil.txt");
+            var destiny = Path.Combine(Environment.CurrentDirectory, 
+                "Globo",
+                "América do Sul",
+                "Brasil",
+                "brasil.txt"
+            );
+
+            MoveFile(origin, destiny);
+
+            var origin_ = Path.Combine(Environment.CurrentDirectory, "brasil.txt");
+            var destiny_ = Path.Combine(Environment.CurrentDirectory, 
+                "Globo",
+                "América do Sul",
+                "Argentina",
+                "argentina.txt"
+            );
+            
+            CopyFile(origin_, destiny_);
+        }
+
+        public static void CreateDirectory()
+        {
+            var path = Path.Combine(Environment.CurrentDirectory, "Globo");
+
+            if (!Directory.Exists(path))
+            {
+                var directory = Directory.CreateDirectory(path);
+
+                var dirAmNorth = directory.CreateSubdirectory("América do Norte");
+                var dirAmCentral = directory.CreateSubdirectory("América Central");
+                var dirAmSouth = directory.CreateSubdirectory("América do Sul");
+
+                dirAmNorth.CreateSubdirectory("USA");
+                dirAmNorth.CreateSubdirectory("México");
+                dirAmNorth.CreateSubdirectory("Canada");
+
+                dirAmCentral.CreateSubdirectory("Costa Rica");
+                dirAmCentral.CreateSubdirectory("Panama");
+
+                dirAmSouth.CreateSubdirectory("Brasil");
+                dirAmSouth.CreateSubdirectory("Argentina");
+                dirAmSouth.CreateSubdirectory("Paraguai");
+            }
+        }
+
+        public static void CreateFile()
+        {
+            var path = Path.Combine(Environment.CurrentDirectory, "brasil.txt");
+            using var sw = File.CreateText(path);
+
+            if (!File.Exists(path))
+            {
+                sw.WriteLine("População: 213MM");
+                sw.WriteLine("IDH: 0,901");
+                sw.WriteLine("Dados atualizados em: 17/03/2023");
+            }
+        }
+
+        public static void CopyFile(string pathOrigin, string pathDestination)
+        {
+            if (!File.Exists(pathOrigin))
+            {
+                System.Console.WriteLine("Erro ao mover arquivo! Arquivo de origem não existe.");
+                
+                return;
+            }
+
+            if (File.Exists(pathDestination))
+            {
+                System.Console.WriteLine($"Arquivo já existente no diretório: {pathDestination}");
+                
+                return;
+            }
+
+            File.Copy(pathOrigin, pathDestination);
+        }
+
+        public static void MoveFile(string pathOrigin, string pathDestination)
+        {
+            if (!File.Exists(pathOrigin))
+            {
+                System.Console.WriteLine("Erro ao mover arquivo! Arquivo de origem não existe.");
+                
+                return;
+            }
+
+            if (File.Exists(pathDestination))
+            {
+                System.Console.WriteLine($"Arquivo já existente no diretório: {pathDestination}");
+
+                return;
+            }
+
+            File.Move(pathOrigin, pathDestination);
+        }
+    }
+}
---
+++ DirectoryRead/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Curso
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var path = "/home/nilsojr/Documentos/Trabalhando com Arquivos e Streams em C#/Directory/Globo";
+
+            // ReadDirectory(path);
+
+            ReadFiles(path);
+
+            System.Console.WriteLine("Digite [ENTER] para finalizar...");
+            Console.ReadLine();
+        }
+
+        public static void ReadFiles(string path)
+        {
+            var files = Directory.GetFiles(path, "*.txt", SearchOption.AllDirectories);
+
+            foreach (var file in files)
+            {
+                var fileInfo = new FileInfo(file);
+                
+                Console.WriteLine($"[Nome]: {fileInfo.Name}");
+                Console.WriteLine($"[Último acesso]: {fileInfo.LastAccessTime}");
+                Console.WriteLine($"[Tamanho]: {fileInfo.Length}");
+                Console.WriteLine($"[Último acesso]: {fileInfo.LastAccessTime}");
+                Console.WriteLine($"[Pasta]: {fileInfo.DirectoryName}");
+
+                System.Console.WriteLine("--------------------");
+            }
+        }
+
+        public static void ReadDirectory(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                var directories = Directory.GetDirectories(path, "*", SearchOption.AllDirectories);
+
+                foreach (var dir in directories)
+                {
+                    var dirInfo = new DirectoryInfo(dir);
+
+                    Console.WriteLine($"[Nome]: {dirInfo.Name}");
+                    Console.WriteLine($"[Raiz]: {dirInfo.Root}");
+
+                    if (dirInfo.Parent != null) Console.WriteLine($"[Pai]: {dirInfo.Parent.Name}");
+
+                    System.Console.WriteLine("--------------------");
+                }
+            } 
+            else
+            {
+                Console.WriteLine($"Diretório {path} não existe!");
+            }
+        }
+    }
+}

dfbc2e07a62d574ce359b648659d686ad7b2492d
Nilso Junior <96146165+Nilso97@users.noreply.github.com>
2023-04-27 10:54:27 -0300

    Estudando a classe String

---
+++ StringReader/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+namespace Curso
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("Caracteres na primeira linha para ler...");
+            sb.AppendLine("Caracteres na segunda linha para ler...");
+            sb.AppendLine("Caracteres na terceira linha para ler...");
+
+            using var sr = new StringReader(sb.ToString());
+            // var buffer = new char[10];
+
+            // var length = 0;
+
+            do
+            {
+                System.Console.WriteLine(sr.ReadLine());
+            } while (sr.Peek() >= 0);
+
+            // do
+            // {
+            //     buffer = new char[10];
+            //     length = sr.Read(buffer);
+            //     Console.Write($"{string.Join("", buffer)}");
+            // } while (length >= buffer.Length);
+
+            System.Console.WriteLine();
+            System.Console.WriteLine("Pressione [ENTER] para finalizar...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StringWriter/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace Curso
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string text = "Lorem ipsum dolor sit amet. Ut architecto atque est consequuntur dolores hic molestias galisum ut neque itaque quo illum assumenda? Ut delectus sint et quam quis qui quas tenetur aut magni debitis! Est galisum aperiam est ipsa mollitia aut recusandae fugiat.\n\n" + 
+            
+            "Et molestiae possimus qui voluptatem dolores cum dolor expedita qui itaque harum qui iste exercitationem. Qui harum nihil et commodi rerum et quia officia aut voluptatem numquam est facilis veritatis et alias illo et itaque nihil.\n\n" + 
+            
+            "Cum debitis rerum aut voluptatum internos aut ipsa quae et galisum iure. Nam dicta ipsa ut omnis natus ad quisquam modi eum corrupti minus.";
+
+            Console.WriteLine($"Texto original: {text}");
+
+            string line, paragraph = null;
+
+            var sr = new StringReader(text);
+
+            while (true)
+            {
+                line = sr.ReadLine();
+                if (line != null)
+                {
+                    paragraph += line + " ";
+                } else 
+                {
+                    paragraph += '\n';
+                    break;
+                }
+            }
+
+            System.Console.WriteLine();
+
+            Console.WriteLine($"Texto modificado: {paragraph}");    
+
+            int characterRead;
+            char convertedCharacter; 
+
+            var sw = new StringWriter();
+            sr = new StringReader(paragraph);    
+
+            while (true)
+            {
+                characterRead = sr.Read();
+                if (characterRead == -1) break;
+
+                convertedCharacter = Convert.ToChar(characterRead);
+
+                if (convertedCharacter == '.') 
+                {
+                    sw.Write("\n\n");
+
+                    sr.Read();
+                    sr.Read();
+                } else
+                {
+                    sw.Write(convertedCharacter);
+                }
+            }
+
+            Console.WriteLine($"Texto armazenado no Stringwriter: {sw.ToString()}");
+
+            System.Console.WriteLine("\n\nDigite [ENTER] para finalizar...");
+            Console.ReadLine();
+        }
+    }
+}

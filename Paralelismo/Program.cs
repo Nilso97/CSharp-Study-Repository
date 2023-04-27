71c6e19f9870ea59583ca57f66ee11e41437b6bb
Nilso Junior <96146165+Nilso97@users.noreply.github.com>
2023-04-27 10:45:29 -0300

    Estudando Paralelismo com .NET

---
+++ Paralelismo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using Paralelismo.Models;
+
+namespace Paralelismo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Paralelismo em .NET: https://www.youtube.com/watch?v=QyJ2Tn_UPjw
+            
+            string[] ceps = new string[5];
+            ceps[0] = "07155081";
+            ceps[1] = "15800100";
+            ceps[2] = "38407369";
+            ceps[3] = "77445100";
+            ceps[4] = "78015818";
+
+            var parallelOptions = new ParallelOptions();
+            parallelOptions.MaxDegreeOfParallelism = 2;
+
+            var stopWatch = new Stopwatch();
+
+            stopWatch.Start();
+
+            var listaCep = new List<CepModel>();
+
+            Parallel.ForEach(ceps, parallelOptions, cep =>
+            {
+                listaCep.Add(new ViaCepService().GetCep(cep));
+            });
+
+            listaCep.OrderBy(cep => cep.Cep)
+                .ToList()
+                .ForEach(cep => Console.WriteLine(cep));
+
+            stopWatch.Stop();
+
+            Console.WriteLine($"\nO tempo de processamento total é de {stopWatch.ElapsedMilliseconds} ms");
+        }
+    }
+}

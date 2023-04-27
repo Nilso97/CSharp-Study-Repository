bab575e54d8123bef9ac6e761845029baae02486
Nilso Junior <96146165+Nilso97@users.noreply.github.com>
2023-04-27 10:55:17 -0300

    Lendo e gravando dados .csv

---
+++ Ler e Gravar dados CSV/Program.cs
@@ -0,0 +1,95 @@
+﻿using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace Curso
+{
+    public class Program
+    {
+        public static List<Venda> vendas;
+
+        public static void Main(string[] args)
+        {
+            CsvReadFile();
+
+            CsvCreateFile();
+            
+            System.Console.WriteLine("Pressione [ENTER] para finalizar");
+
+            Console.Read();
+        }
+
+        public static void CsvReadFile()
+        {
+            var path = Path.Combine(Environment.CurrentDirectory, "Data.csv"); 
+
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = true
+            };
+
+            using (var reader = new StreamReader(path))
+            
+            using (var csv = new CsvReader(reader, config))
+            {
+                csv.Context.RegisterClassMap<VendaMap>();
+
+                vendas = csv.GetRecords<Venda>().ToList();
+
+                foreach (var venda in vendas)
+                {
+                    Console.WriteLine($"Cliente: {venda.NomeCliente}, Pacote: {venda.NomePacote}, Data de nascimento: {venda.DataNascimento}, Data de atendimento: {venda.DataAtendimento}, Cidade e Estado: {venda.CidadeEstado}");
+
+                    System.Console.WriteLine("--------------------\n");
+                }
+            }
+        }
+
+        public static void CsvCreateFile()
+        { 
+            var path = Path.Combine(
+                Environment.CurrentDirectory, 
+                "Arquivos"
+            );
+
+            var directory = new DirectoryInfo(path);
+
+            if (!directory.Exists)
+            {
+                System.Console.WriteLine($"Criando diretório localizado em {path}\n");
+
+                Thread.Sleep(3000);
+
+                directory.Create();
+            }
+            else
+            {
+                Console.WriteLine($"Diretório [{path}] já existe!\n");
+            }
+
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = true
+            };
+
+            System.Console.WriteLine("Criando o arquivo CSV...\n");
+
+            Thread.Sleep(3000);
+
+            using (var writer = new StreamWriter($"{path}/dados-vendas.csv"))
+
+            using (var csv = new CsvWriter(writer, config)) 
+            {
+                csv.Context.RegisterClassMap<VendaMap>();
+
+                csv.WriteHeader<Venda>();
+
+                csv.NextRecord();
+
+                csv.WriteRecords(vendas);
+            }
+
+            System.Console.WriteLine("Arquivo criado com sucesso!\n");
+        }
+    }
+}
---
+++ Ler e Gravar dados CSV/VendaMap.cs
@@ -0,0 +1,17 @@
+using CsvHelper.Configuration;
+
+namespace Curso
+{
+    public class VendaMap : ClassMap<Venda>
+    {
+        public VendaMap()
+        {
+            Map(v => v.Id).Name("id");
+            Map(v => v.NomeCliente).Name("nome_cliente");
+            Map(v => v.NomePacote).Name("nome_pacote");
+            Map(v => v.DataNascimento).Name("data_nascimento");
+            Map(v => v.DataAtendimento).Name("data_atendimento");
+            Map(v => v.CidadeEstado).Name("cidade_estado");
+        }
+    }
+}
